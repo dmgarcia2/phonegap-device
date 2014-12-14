b3f88a989ae051235e4bb61743f8460addf0347a
David Martín García <david@iasmobile.es>
2014-12-14 19:37:38 +0100

    Add plugins: status bar, device and network-information Web application based on jQuery Mobile

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,47 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
+"use strict";
+
+var application = {};
+
+application.app = {
+	
     initialize: function() {
-        this.bindEvents();
+		document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        StatusBar.hide();
+		FastClick.attach(document.body);
+        
+        // Obteniendo información del dispositivo
+        $('#device-name').html(device.name);
+        $('#device-cordova').html(device.cordova);
+        $('#device-platform').html(device.platform);
+        $('#device-uuid').html(device.uuid);
+        $('#device-version').html(device.version);
+        
+        switch (navigator.network.connection.type) {
+            case Connection.UNKNOWN:
+                $('#device-network').html('Desconocido');
+                break;
+            case Connection.ETHERNET:
+                $('#device-network').html('Ethernet');
+                break;
+            case Connection.WIFI:
+                $('#device-network').html('WiFi');
+                break;
+            case Connection.CELL_2G:
+                $('#device-network').html('GSM (2G)');
+                break;
+            case Connection.CELL_3G:
+                $('#device-network').html('3G');
+                break;
+            case Connection.CELL_4G:
+                $('#device-network').html('4G');
+                break;
+            case Connection.NONE:
+                $('#device-network').html('Sin conexión');
+                break;
+        }
     }
+
 };
